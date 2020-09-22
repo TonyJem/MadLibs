d6e8578e9a84c4d7d972abdd00a31b9477a0b08a
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-22 10:22:18 +0300

    Add singularNounCombo element to UI

--- MadLibs/ViewController.swift
+++ MadLibs/ViewController.swift
@@ -10,14 +10,18 @@ import Cocoa
 class ViewController: NSViewController {
 
     @IBOutlet weak var pastTenseVerbTextField: NSTextField!
-    
+    @IBOutlet weak var singularNounCombo: NSComboBox!
+    fileprivate let singularNouns = ["dog", "muppet", "ninja", "pirate", "dev" ]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         pastTenseVerbTextField.stringValue = "ate"
         
+        singularNounCombo.removeAllItems()
+        singularNounCombo.addItems(withObjectValues: singularNouns)
+        singularNounCombo.selectItem(at: singularNouns.count - 1)
     }
 
     override var representedObject: Any? {
