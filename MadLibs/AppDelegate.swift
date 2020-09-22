f75dd98482c504302d02af28134806c1d79f323c
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-22 09:16:33 +0300

    Initial Commit

---
+++ MadLibs/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MadLibs
+//
+//  Created by Anton on 2020-09-22.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MadLibs/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MadLibs
+//
+//  Created by Anton on 2020-09-22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
